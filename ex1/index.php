ac8b4ad634f52e52412a9305de3f4a6ea66586e8
AZIMBOY4050 <azimboykamiljanov@gmail.com>
2025-05-27 12:59:41 +0500

    added p

--- ex1/index.php
+++ ex1/index.php
@@ -22,6 +22,9 @@
 
     <p class="register-link">
         Not registered yet? <a href="register.php">Click here</a>
+    </p> 
+    <p class="register-link">
+        Not registered yet? <a href="register.php">Click here</a>
     </p>
 </div>
 
---
+++ user.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <label for="price">unit price</label>
+        <input type="text"name="price" id="price" placeholder="price" required>
+        <label for="qyt">Quentity</label>
+        <input type="text"name="qyt" id="qyt" placeholder="qyt" required>
+        <button type="submit">Add to Chart</button>
+
+    </div>
+
+</body>
+</html>
