d9ac7c7a9a19efd7205b9a48a9f13d06a7e37df1
AZIMBOY4050 <azimboykamiljanov@gmail.com>
2025-05-27 14:09:06 +0500

    added

--- ex.sql
+++ ex.sql
@@ -10,7 +10,20 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
+-- piddi bola ecbiwqdhjwcebiwcnvcbrfvebwscdevddddhdvbhvdbvhdbvhevbhevbeakcwe;vqwncebvkjd;qwfbrvcebg
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
--- ex1/index.php
+++ ex1/index.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Online Shop - Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/index.css">
 </head>
+
 <body>
 
-<div class="form-wrapper">
-    <h2>Login</h2>
-    <form method="post" action="log.php">
-        <label for="username">Username</label>
-        <input id="username" type="text" name="username" placeholder="Enter your username" required>
+    <div class="form-wrapper">
+        <h2>Login</h2>
+        <form method="post" action="log.php">
+            <label for="username">Username</label>
+            <input id="username" type="text" name="username" placeholder="Enter your username" required>
 
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password" placeholder="Enter your password" required>
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" placeholder="Enter your password" required>
 
-        <button type="submit">Login</button>
-    </form>
+            <button type="submit">Login</button>
+        </form>
 
-    <p class="register-link">
-        Not registered yet? <a href="register.php">Click here</a>
-    </p> 
- 
-     
-</div>
+        <p class="register-link">
+            Not registered yet? <a href="register.php">Click here</a>
+        </p>
+
+        <p class="register-link">
+            Not registered yet? <a href="register.php">Click here</a>
+        </p>
+    </div>
 
 </body>
+
 </html>
